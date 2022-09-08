e3142005147bcb2ee1ba3cb28228639f98dfefee
dmonh3h3 <adhymasfs@gmail.com>
2022-09-08 14:42:21 +0700

    first commit

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Mahasiswa extends CI_Controller
+{
+    public function index()
+    {
+        echo 'index';
+    }
+    public function buka()
+    {
+        echo 'buka';
+    }
+}
